86f7a34738b61353deacc2e144a0137fe6844485
faelzin011 <rafaelvasonceloscoutinho@gmail.com>
2024-06-07 22:31:11 -0300

    lista7

---
+++ listas/lista7/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args){
+        Usuario usuario = new Usuario("Rafael", "rafaelzin@gmail.com");
+        usuario.exibirInfo();      
+    }
+}
---
+++ listas/lista7/escola/Usuario.java
@@ -0,0 +1,30 @@
+public class Usuario {
+    private String nome;
+    private String email;
+
+    public Usuario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public void exibirInfo(){
+        System.out.println("Nome:" + this.nome);
+        System.out.println("Email:" + this.email);
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+}
